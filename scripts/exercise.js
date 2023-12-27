f6aef5f1fa989c5798506ac6286846d940774785
Aneta Falkowska <aneta.falkowska@gmail.com>
2023-12-27 17:38:02 +0100

    Add countdown feature

--- scripts/exercise.js
+++ scripts/exercise.js
@@ -1,7 +1,5 @@
 import { currentExercise } from "../data/exercises.js";
 
-console.log(currentExercise)
-
 function convertToTimeString(seconds) {
   const stringMinutes = Math.floor(seconds / 60);
   const stringSeconds = seconds % 60;
@@ -18,16 +16,66 @@ document.querySelector(".js-phase-duration").innerHTML = convertToTimeString(
 );
 
 function countDown({ id, durationInSeconds }) {
-  document.querySelector(".js-phase-name").innerHTML = id;
-  let i = durationInSeconds;
-  const identifier = setInterval(() => {
+  return new Promise((res) => {
+    document.querySelector(".js-phase-name").innerHTML = id;
+    let i = durationInSeconds;
     document.querySelector(".js-phase-duration").innerHTML =
       convertToTimeString(i);
-    i--;
-    if (i < 0) {
-      clearInterval(identifier);
-    }
-  }, 1000);
+    const identifier = setInterval(() => {
+      i--;
+      if (i < 0) {
+        clearInterval(identifier);
+        res();
+      } else {
+        document.querySelector(".js-phase-duration").innerHTML =
+          convertToTimeString(i);
+      }
+    }, 1000);
+  });
 }
 
-countDown(currentExercise[1]);
+function createPhasesArray(exercise) {
+  const array = [];
+  const numberOfSets = exercise[4].sets;
+  for (let i = 0; i < numberOfSets; i++) {
+    array.push(exercise[1], exercise[2]);
+  }
+  return [exercise[0], ...array, exercise[3]];
+}
+
+const phasesArray = createPhasesArray(currentExercise);
+console.log(phasesArray);
+
+
+const countDownArray = phasesArray.map((el) => {
+  const newFunction = () => {
+    return countDown({ id: el.id, durationInSeconds: el.durationInSeconds });
+  };
+  return newFunction;
+});
+
+console.log(countDownArray);
+
+countDownArray
+  .reduce((chain, countDownFn) => chain.then(countDownFn), Promise.resolve())
+  .then(() => alert("koniec"));
+
+
+// const prepareCountDown = () => {
+//   return countDown(currentExercise[0]);
+// };
+// const workCountDown = () => {
+//   return countDown(currentExercise[1]);
+// };
+// const restCountDown = () => {
+//   return countDown(currentExercise[2]);
+// };
+// const cooldownCountDown = () => {
+//   return countDown(currentExercise[3]);
+// };
+
+// prepareCountDown()
+//   .then(workCountDown)
+//   .then(restCountDown)
+//   .then(cooldownCountDown)
+//   .then(() => alert("koniec"));
